1a1fb8f8395669dcefc83bc178534f6b313ac7be
かがみ <seki@kagami.me>
2019-11-12 23:21:25 +0800

    feat: add rollup

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+import typescript from 'rollup-plugin-typescript'
+import resolve from 'rollup-plugin-node-resolve'
+import builtins from 'builtin-modules'
+import commonjs from 'rollup-plugin-commonjs'
+import { terser } from 'rollup-plugin-terser'
+
+export default  {
+  input: './index.ts',
+  plugins: [
+    typescript(),
+    resolve(),
+    commonjs(),
+    terser({
+      output: {
+        comments: 'all',
+      },
+    }),
+  ],
+  output: {
+    file: 'dist/main.js',
+    format: 'cjs',
+    sourcemap: true,
+  },
+  external: builtins,
+}
